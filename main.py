9ffafcbdf4860bdbaf5ca50aa80f8bd81121e851
NazarovDA <d.n.0@bk.ru>
2022-06-10 00:21:21 +0300

    users fetched, roles applying added but untested

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 # coding utf-8
 
 import discord
-from discord import Member, VoiceState, Guild, VoiceChannel
+from discord import Member, VoiceState, Guild, VoiceChannel, TextChannel, Role
 
 import asyncio
 
 import settings
+import datetime
 
 class Client(discord.Client):
-    channelToCreateTempChannelsId: int = 715965021569548344
+    channelToCreateTempChannelsId: int = settings.__TEMP_CHANNEL_CREATING_CHANNEL_ID__
     temporaryChannels: list[VoiceChannel] = list()
 
     async def on_ready(self):
         print(f"Logged as {self.user}")
+        # await self.__check_milestones()
+        if not self.intents.members: return
+        guild: Guild = self.guilds[0]
+
+        async for member in guild.fetch_members():
+            print(member)
 
     async def on_message(self, message: discord.Message):
         print(message)
@@ -42,7 +49,35 @@ class Client(discord.Client):
 
             self.temporaryChannels.append(new_channel)
 
+    # ----- role adding logic -----
+    async def on_member_join(self, member: Member):
+        if not discord.Intents.members or not discord.Permissions.manage_roles: return
+
+        welcome_channel: TextChannel = member.guild.get_channel(settings.__GREETINGS_CHANNEL_ID__)
+        await welcome_channel.send(f"Hello there {member.nick}")
+
+        await member.add_roles(
+            roles=[member.guild.get_role(settings.__NEW_MEMBER_ROLE_ID__)], reason=f"New member {member.nick} has joined"
+        )
+
+    async def __check_milestones(self):
+        """
+        adding role for passing milestiones
+        """
+        milestones: tuple[datetime.timedelta, datetime.timedelta, Role]
+
+        for member in self.guilds[0].members:
+            member: Member
+            for _from, _to, _role in milestones:
+                if _from < member.joined_at - datetime.datetime.now() <= _to:
+                    member.add_roles(_role)
+                    member.remove_roles([obj[2] for obj in milestones].remove(_role))
+
+
     
 if __name__ == "__main__":
-    client = Client()
+    intents = discord.Intents.default()
+    intents.members = True
+
+    client = Client(intents=intents)
     client.run(settings.__TOKEN__)
